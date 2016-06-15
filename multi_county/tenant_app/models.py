384a4aff5c3cce6b630b80525f9636458c8e1c41
anushamc <anushamc@yahoo.com>
2016-06-15 03:40:54 +0100

    create tenant_app model

--- multi_county/tenant_app/models.py
+++ multi_county/tenant_app/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Accident(models.Model):
+    accident_index = models.CharField(max_length=20)
+    location_easting_osgr = models.IntegerField()
+    location_northing_osgr = models.IntegerField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    police_force = models.IntegerField()
+    accident_severity = models.IntegerField()
+    number_of_vehicles = models.IntegerField()
+    number_of_casualties = models.IntegerField()
+    date = models.DateField()
+    day_of_week = models.IntegerField()
+    time = models.TimeField()
+    # GeoDjango-specific: a geometry field (PointField)
+    geom = models.PointField(srid=27700, null=True)
+    objects = models.GeoManager()
