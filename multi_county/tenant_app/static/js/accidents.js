0fc326379b1c812199a633fa53b9c4eb46b623a7
anushamc <anushamc@yahoo.com>
2016-06-15 06:48:53 +0100

    bugfixes

--- multi_county/tenant_app/static/js/accidents.js
+++ multi_county/tenant_app/static/js/accidents.js
@@ -23,7 +23,47 @@
         layers:[accident]
       });
 
+
+      var container = document.getElementById('popup');
+      var content = document.getElementById('popup-content');
+      var closer = document.getElementById('popup-closer');
+
+      /**
+       * Create an overlay to anchor the popup to the map.
+       */
+      var overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
+        element: container,
+        autoPan: true,
+        autoPanAnimation: {
+          duration: 250
+        }
+      }));
+
+      /**
+       * Add a click handler to hide the popup.
+       * @return {boolean} Don't follow the href.
+       */
+      closer.onclick = function() {
+        overlay.setPosition(undefined);
+        closer.blur();
+        return false;
+      };
+      var overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
+        element: container,
+        autoPan: true,
+        autoPanAnimation: {
+          duration: 250
+        }
+      }));
+
+      map.addOverlay(overlay);
+
       map.addInteraction(select);
       select.on('select', function(e) {
-        console.log(selected);
+        var feature = e.target.getFeatures().getArray()[0];
+        if(feature){
+          var coordinates = feature.getGeometry().getFirstCoordinate();
+          overlay.setPosition(coordinates);
+          content.innerHTML = "Accident Stats <br/> Number of Vehicles:"+feature.get('number_of_vehicles')+"<br/> Date:"+feature.get('date');
+        }
       });
